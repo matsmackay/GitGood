ff56e4da7780e0766a8bd1f79e1ec5bda949817c
Mats Mackay <matsmackay@live.nl>
2019-08-12 14:18:34 +0200

    no message

--- Git_Good/Git_Good/Classes.py
+++ Git_Good/Git_Good/Classes.py
@@ -5,9 +5,12 @@ class User:
         self.surname = surname
         self.name = name
         self.fullname = self.surname + " " + self.name
+        self.user_id = self.surname + self.name
 
     def return_full_name(self):
         print("Your username is {}.".format(self.fullname))
+        print("Your user ID is {}.".format(self.user_id))
+
 
 # Commented out for later use
 # surname = input('What is your surname?: ').upper()
