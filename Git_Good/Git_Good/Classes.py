53be5add2b6eab956da8c4b460e53f6e8e164225
knicolai <knicolai@deloitte.nl>
2019-08-12 14:52:45 +0200

    removed print statements

--- Git_Good/Git_Good/Classes.py
+++ Git_Good/Git_Good/Classes.py
@@ -9,10 +9,6 @@ class User:
         self.fullname = self.surname + " " + self.name
         self.user_id = self.surname + self.name
 
-    def return_full_name(self):
-        print("Your username is {}.".format(self.fullname))
-        print("Your user ID is {}.".format(self.user_id))
-
 
 # Define class Order that creates an order
 class Order:
